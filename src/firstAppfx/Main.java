812fa61f5b397977baf98eee39b4850be3731e23
Rafael Amador <rafael.amador24@example.com>
2025-11-14 14:15:15 -0600

    AnonymousInnerClasses_Lambda

--- src/firstAppfx/Main.java
+++ src/firstAppfx/Main.java
@@ -13,7 +13,7 @@ import javafx.scene.layout.VBox;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+public class Main extends Application{
     Button button1;
     Button button2;
     public static void main(String[] args) {
@@ -25,8 +25,19 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         stage.setTitle("First_App");
         button1 = new Button("Click me");
         button2 = new Button("Click me");
-        button1.setOnAction(this);
-        button2.setOnAction(this);
+        //------------------------------
+        // Anonymous Inner Classes     |
+        //------------------------------
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                System.out.println("Button 2 pressed");
+            }
+        });
+        //------------------------------
+        // Lambda Expression (Shortcut)|
+        //------------------------------
+        button2.setOnAction(e -> System.out.println("Button 1 pressed"));
 
         VBox layout = new VBox();
         layout.getChildren().addAll(button1, button2);
@@ -34,16 +45,6 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         Scene scene = new Scene(layout, 500, 250);
         stage.setScene(scene);
         stage.show();
-    }
-
-    @Override //needed from the implement EventHandler
-    public void handle(ActionEvent event){
-        if(event.getSource() == button1){
-            System.out.println("Button1 was clicked");
-        }
-        else if (event.getSource() == button2){
-            System.out.println("Button2 was clicked");
-        }
     }
     //Parent root = FXMLLoader.load(getClass().getResource("firstAppfx.fxml"));
     //stage.setScene(new Scene(root, 600, 400));
