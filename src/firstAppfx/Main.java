f341c47b4b1083a7d2ef4035fa7991c0338f9dfc
Rafael Amador <rafael.amador24@example.com>
2025-11-14 14:00:33 -0600

    VBox_2Buttons

--- src/firstAppfx/Main.java
+++ src/firstAppfx/Main.java
@@ -8,11 +8,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 
 public class Main extends Application implements EventHandler<ActionEvent> {
-    Button button;
+    Button button1;
+    Button button2;
     public static void main(String[] args) {
         launch(args);
     }
@@ -20,11 +23,13 @@ public class Main extends Application implements EventHandler<ActionEvent> {
     @Override
     public void start(Stage stage) throws Exception{
         stage.setTitle("First_App");
-        button = new Button("Click me");
-        button.setOnAction(this);
+        button1 = new Button("Click me");
+        button2 = new Button("Click me");
+        button1.setOnAction(this);
+        button2.setOnAction(this);
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        VBox layout = new VBox();
+        layout.getChildren().addAll(button1, button2);
 
         Scene scene = new Scene(layout, 500, 250);
         stage.setScene(scene);
@@ -33,8 +38,11 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 
     @Override //needed from the implement EventHandler
     public void handle(ActionEvent event){
-        if(event.getSource() == button){
-            System.out.println("Button was clicked");
+        if(event.getSource() == button1){
+            System.out.println("Button1 was clicked");
+        }
+        else if (event.getSource() == button2){
+            System.out.println("Button2 was clicked");
         }
     }
     //Parent root = FXMLLoader.load(getClass().getResource("firstAppfx.fxml"));
