626a0c45a40076193873e58e288f7a6a24da71ad
Rafael Amador <rafael.amador24@example.com>
2025-11-14 14:26:05 -0600

    Switching Scenes

--- src/firstAppfx/Main.java
+++ src/firstAppfx/Main.java
@@ -7,44 +7,42 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 
 public class Main extends Application{
-    Button button1;
-    Button button2;
+    
+    Scene scene1, scene2;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception{
-        stage.setTitle("First_App");
-        button1 = new Button("Click me");
-        button2 = new Button("Click me");
-        //------------------------------
-        // Anonymous Inner Classes     |
-        //------------------------------
-        button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event){
-                System.out.println("Button 2 pressed");
-            }
-        });
-        //------------------------------
-        // Lambda Expression (Shortcut)|
-        //------------------------------
-        button2.setOnAction(e -> System.out.println("Button 1 pressed"));
+    public void start(Stage window) throws Exception{
+        Label label1 = new Label("Welcome to the 1st scene");
+        Label label2 = new Label("Welcome to the 2nd scene");
+        Button button1 = new Button("Go to scene 2");
+        Button button2 = new Button("Go to scene 1");
+        button1.setOnAction(e -> window.setScene(scene2));
+        button2.setOnAction(e -> window.setScene(scene1));
 
-        VBox layout = new VBox();
-        layout.getChildren().addAll(button1, button2);
+        //Layout 1 - Vertical Column
+        VBox layout1 = new VBox();
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 500, 500);
 
-        Scene scene = new Scene(layout, 500, 250);
-        stage.setScene(scene);
-        stage.show();
+        //Layout 2 
+        VBox layout2 = new VBox();
+        layout2.getChildren().addAll(label2, button2);
+        scene2 = new Scene(layout2, 800, 300);
+
+        window.setScene(scene1);
+        window.setTitle("App");
+        window.show();
     }
     //Parent root = FXMLLoader.load(getClass().getResource("firstAppfx.fxml"));
     //stage.setScene(new Scene(root, 600, 400));
